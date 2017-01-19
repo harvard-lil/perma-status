d8d924b95b92dedbcdb3e8cf4a28809c1f4df6d7
Ben Steinberg <bsteinberg@law.harvard.edu>
2017-01-19 17:17:38 -0500

    Add intervals, use total_seconds

--- perma_times.py
+++ perma_times.py
@@ -15,16 +15,17 @@ objects = []
 (objs, next_url) = lookup(url)
 
 now = datetime.now(tz=tz.tzutc())
-
+last = now
 for obj in objs:
     timestamp = datetime.strptime(obj['creation_timestamp'], "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo=tz.tzutc())
     delta = now - timestamp
-    triplet = (timestamp, delta.seconds, obj['queue_time'], obj['capture_time'])
-    objects.append(triplet)
+    interval = last - timestamp
+    last = timestamp
+    objects.append((timestamp, delta.total_seconds(), obj['queue_time'], obj['capture_time'], interval.total_seconds()))
 
-print("most recent capture was {seconds} seconds ago".format(seconds=objects[0][1]))
-print("most recent completed capture was {seconds} seconds ago".format(seconds=filter(lambda x: x[3] is not None, objects)[0][1]))
-print("twentieth capture ago was {seconds} seconds ago".format(seconds=objects[-1][1]))
+print("most recent capture was {seconds:.0f} seconds ago".format(seconds=objects[0][1]))
+print("most recent completed capture was {seconds:.0f} seconds ago".format(seconds=filter(lambda x: x[3] is not None, objects)[0][1]))
+print("twentieth capture ago was {seconds:.0f} seconds ago".format(seconds=objects[-1][1]))
 
 queue_times = [x[2] for x in objects if x[2] is not None]
 print("mean queue time over the last twenty captures is {mean:.2f} seconds".format(mean=sum(queue_times) / float(len(queue_times))))
@@ -32,3 +33,6 @@ capture_times = [x[3] for x in objects if x[3] is not None]
 print("mean capture time over the last twenty captures is {mean:.2f} seconds".format(mean=sum(capture_times) / float(len(capture_times))))
 
 print("number of unfinished captures in the last twenty is {number}".format(number=len([x for x in objects if x[3] is None])))
+
+print("maximum interval between last twenty captures is {max:.0f} seconds".format(max=max([x[4] for x in objects])))
+print("minimum interval between last twenty captures is {min:.0f} seconds".format(min=min([x[4] for x in objects])))
