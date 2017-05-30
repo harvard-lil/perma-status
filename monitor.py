8c5a502a225aaec521966065874202f8fd24e13d
Ben Steinberg <bsteinberg@law.harvard.edu>
2017-05-30 12:10:47 -0400

    Database maintenance off

--- monitor.py
+++ monitor.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def perma_status(up="down.", message="Perma is undergoing database maintenance and will be up shortly."):
+def perma_status(up="up!", message=None):
     """
     Show the status page with charts
 
